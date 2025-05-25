1a9285c01fce8343d045b6657d642a1e7d2375b6
Angelo <angelo.lavarini@studenti.unipd.it>
2025-05-25 17:15:42 +0200

    Added recyclerview with all options in the settings fragment; added windowInsetsManagement in SettingsFragment.kt . Added functionality for the share button.

--- app/src/main/java/it/lavorodigruppo/flexipdf/fragments/SettingsFragment.kt
+++ app/src/main/java/it/lavorodigruppo/flexipdf/fragments/SettingsFragment.kt
@@ -5,53 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import it.lavorodigruppo.flexipdf.R
+import it.lavorodigruppo.flexipdf.adapters.SettingsAdapter
+import it.lavorodigruppo.flexipdf.items.SettingsItem
+import it.lavorodigruppo.flexipdf.data.SettingsDatasource
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
+    private lateinit var settingsRecyclerView: RecyclerView
+    private lateinit var settingsAdapter: SettingsAdapter
+    private lateinit var settingsList: List<SettingsItem>
 
-        }
-    }
+    private var originalBannerPaddingTop = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
+        settingsRecyclerView = view.findViewById(R.id.settingsRecyclerView)
+        settingsRecyclerView.setHasFixedSize(true)
+        settingsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-                }
-            }
+        val dataSource = SettingsDatasource(requireContext())
+        settingsList = dataSource.getSettingsOptions()
+
+        settingsAdapter = SettingsAdapter(settingsList)
+        settingsRecyclerView.adapter = settingsAdapter
+
+        // --- WindowInsets management ---
+        val bannerContentLayout = view.findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.bannerContentLayout)
+
+        originalBannerPaddingTop = bannerContentLayout.paddingTop
+
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            bannerContentLayout.setPadding(
+                bannerContentLayout.paddingLeft,
+                originalBannerPaddingTop + systemBarsInsets.top,
+                bannerContentLayout.paddingRight,
+                bannerContentLayout.paddingBottom
+            )
+
+            settingsRecyclerView.setPadding(
+                settingsRecyclerView.paddingLeft,
+                settingsRecyclerView.paddingTop,
+                settingsRecyclerView.paddingRight,
+                systemBarsInsets.bottom
+            )
+
+            insets
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/it/lavorodigruppo/flexipdf/items/SettingsItem.kt
@@ -0,0 +1,6 @@
+package it.lavorodigruppo.flexipdf.items
+
+data class SettingsItem(
+    val title: String,
+    val iconResId: Int
+)
